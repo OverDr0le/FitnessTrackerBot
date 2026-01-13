1db6f5f5bf0e2aaff22d8121e64fe4a5586471b4
Daniil Yakusik <dvyakusik@edu.hse.ru>
2026-01-13 23:05:58 +0300

    init bot

---
+++ bot.py
@@ -0,0 +1,21 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+from configs.config_reader import config
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token = config.bot_token.get_secret_value())
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message:types.Message):
+    await message.answer("Привет! Я фитнесс трекер, помогаю следить за питанием!")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
