2edb7bddd77a8e997336c2e08723b781fb3d92d9
Daniil Yakusik <dvyakusik@edu.hse.ru>
2026-01-14 23:29:05 +0300

    menu handler

---
+++ handlers/menu.py
@@ -0,0 +1,16 @@
+from aiogram.filters.command import Command
+from aiogram import Router
+from aiogram.types import Message
+
+from keyboards.menu_kb import menu_kb
+
+
+router = Router()
+
+@router.message(Command("/menu"))
+async def cmd_start(message: Message):
+    await message.answer(
+        text = "Выберите действие",
+        reply_markup= menu_kb()
+    )
+
