5506f8cb5463e22708b0605e338e9f31d0eaeb50
Daniil Yakusik <dvyakusik@edu.hse.ru>
2026-01-14 22:43:15 +0300

    bot with common handler

--- bot.py
+++ bot.py
@@ -1,21 +1,23 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
 
+from aiogram import Bot, Dispatcher
 from configs.config_reader import config
+from handlers import common
 
-logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token = config.bot_token.get_secret_value())
-dp = Dispatcher()
-
-@dp.message(Command("start"))
-async def cmd_start(message:types.Message):
-    await message.answer("Привет! Я фитнесс трекер, помогаю следить за питанием!")
 
 async def main():
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=config.bot_token.get_secret_value())
+    dp = Dispatcher()
+
+    dp.include_routers(
+        common.router
+    )
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
